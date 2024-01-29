b8baff65085bc4c14e6204649d81f38c03b46200
Kyuwoong Lee <kwooler@gmail.com>
2024-01-28 22:52:01 -0800

    Initial commit

---
+++ main.hpp
@@ -0,0 +1,14 @@
+// Find the sub string in the original string
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int finduserstring(char[], int, char[], int);
+
+int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
+{
+	/*
+	Make your code
+	*/
+}
