e71489853ece398e30f0484f385da17f3d7b9002
Ernesto Flores <eflores73@students.avc.edu>
2024-03-12 14:24:44 -0700

    Fial

--- main.hpp
+++ main.hpp
@@ -10,25 +10,34 @@ int mycstrcat(char[], char[]);
 
 int mycstrlen(char src[])
 {
-	/***************************************************
-	 * Do not use string class type
-	 * Use onlye character array type
-	 * Code your program here
-	 ***************************************************/
+	int cnt = 0;
+	while (*src != '\0'){
+		src++;
+		cnt++;
+	}
+	return cnt;
 }
 int mycstrcpy(char src[], char target[])
 {
-	/***************************************************
-	 * Do not use string class type
-	 * Use onlye character array type
-	 * Code your program here
-	 ***************************************************/
+	int cnt = 0;
+	while (src[cnt] != '\0') {
+		target[cnt] = src[cnt];
+		cnt++;
+	}
+	target[cnt] = '\0';
+	return cnt;
 }
 int mycstrcat(char src[], char target[])
 {
-	/***************************************************
-	 * Do not use string class type
-	 * Use onlye character array type
-	 * Code your program here
-	 ***************************************************/
+	int cnt1=0;
+	while(src[cnt1] != '\0')
+		cnt1++;
+
+	int cnt2 = 0;
+	while(target[cnt2] != '\0'){
+		src[cnt1++] = target [cnt2++];
+	}
+	src[cnt1] = '\0';
+	return cnt1;
+
 }
